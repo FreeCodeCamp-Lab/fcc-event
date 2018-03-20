3c5e51d01b4b9d9b1901535ef7ce09fe959eb545
captainblue2013 <admin@lanhao.name>
2018-03-20 11:22:17 +0800

    db presets

--- models/Attachment.js
+++ models/Attachment.js
@@ -1,10 +1,9 @@
 'use strict';
-const {Field, Table, Migrate} = require('xiaolan-db');
+const {Field, Table, Migrate,Presets} = require('xiaolan-db');
 
 module.exports = new Table('attachment', {
-  id:Field.name('id').bigint(true).primary().AI().comment('id'),
+  ...Presets.AI,
   eventID: Field.name('event_id').bigint(true).index().comment('活动ID'),
   path: Field.name('path').varchar(1024).comment('地址'),
-  createTime: Field.name('create_time').bigint(true).index(),
-  updateTime: Field.name('update_time').bigint(true).index()
+  ...Presets.opTime,
 });
--- models/Submit.js
+++ models/Submit.js
@@ -1,12 +1,11 @@
 'use strict';
-const {Field, Table, Migrate} = require('xiaolan-db');
+const { Field, Table, Migrate, Presets } = require('xiaolan-db');
 
 module.exports = new Table('submit', {
-  id:Field.name('id').bigint(true).primary().AI().comment('id'),
+  ...Presets.AI,
   eventID: Field.name('event_id').bigint(true).index().uniq('event_user').comment('活动ID'),
   userID: Field.name('user_id').bigint(true).index().uniq('event_user').comment('用户ID'),
   status: Field.name('status').tinyint(true).default(0).index().comment('0初始化 1签到后'),
   submitTime: Field.name('submit_time').bigint(true).comment('报名时间'),
-  createTime: Field.name('create_time').bigint(true).index(),
-  updateTime: Field.name('update_time').bigint(true).index()
+  ...Presets.opTime,
 });
